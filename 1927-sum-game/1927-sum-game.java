1f283f489f03754659a2addb982a0e0500fa93d9
Utkarsh raj <utkarshr571@gmail.com>
2026-08-23 23:09:43 +0530

    Time: 8 ms (53.28%), Space: 46.5 MB (97.81%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int length=num.length();
+        int leftQuestionMarks=0;
+        int rightQuestionMarks=0;
+        int leftSum=0;
+        int rightSum=0;
+
+        for(int i=0;i<length/2;i++){
+            if(num.charAt(i)=='?')
+            leftQuestionMarks++;
+            else
+            leftSum+=num.charAt(i)-'0';
+        }
+
+        for(int i=length/2;i<length;i++){
+            if(num.charAt(i)=='?')
+            rightQuestionMarks++;
+            else
+            rightSum+=num.charAt(i)-'0';
+        }
+
+        if((leftQuestionMarks+rightQuestionMarks)%2==1)
+        return true;
+
+        return leftSum-rightSum!=9*(rightQuestionMarks-leftQuestionMarks)/2;
+    }
+}
